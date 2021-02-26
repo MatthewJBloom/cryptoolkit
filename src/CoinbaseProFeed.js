d4c75f1a962bdd6a7ebdcd78f13473f31521d7ba
Matthew Bloom <matthew.j.bloom@gmail.com>
2021-02-26 15:28:46 -0500

    convert to standalone class

--- src/CoinbaseProFeed.js
+++ src/CoinbaseProFeed.js
@@ -1,47 +1,83 @@
-import React, { Component } from 'react'
 const WebSocket = require('websocket').w3cwebsocket
-//const EventEmitter = require('events')
+const EventEmitter = require('events')
 
-const client = new WebSocket("wss://ws-feed.pro.coinbase.com")
+
+// Set subscription details
 const subscription = {
       type: "subscribe",
       product_ids: ["BTC-USD"],
       channels: [{name: "ticker"}]
     }
+
 /**
  * Represents a CoinbasePro Websocket feed
  *
  */
-class CoinbaseProFeed extends Component {
+class CoinbaseProFeed {
   constructor() {
-    super()
-    this.state = {price: 0}
-  }
-  componentDidMount() {
-    client.onopen = () => {
-      console.log('client connected')
-      client.send(JSON.stringify(subscription))
+    // Create websocket client
+    this.client = new WebSocket("wss://ws-feed.pro.coinbase.com")
+    // Create price event emitter for outputting price updates
+    this.priceEventEmitter = new EventEmitter()
+
+    // Handle client events
+    this.client.onopen = () => {
+      console.log('CoinbaseProFeed WebSocket client connected')
+      // Send subscription (required within 60s of connection)
+      this.client.send(JSON.stringify(subscription))
     }
-    client.onmessage = (message) => {
+    this.client.onmessage = (message) => {
+      // console.log(message)
       let data = JSON.parse(message.data)
-      this.setState(state => ({
-        price: data.price
-      }))
-      console.log(data.price)
+      let type = data.type
+      switch (type) {
+        case 'subscriptions':
+          // this.printSubscriptions(data)
+          break
+        case 'ticker':
+          // this.printTicker(data)
+          // Emit price event to priceEventEmitter
+          this.priceEventEmitter.emit('price', data.price)
+          break
+        default:
+          console.warn('UNIMPLEMENTED TYPE CASE:', type)
+      }
     }
-    client.onclose = () => {
-      console.log('client closed')
+    this.client.onclose = () => {
+      console.error('CoinbaseProFeed WebSocket client closed')
     }
-  }
+  } // constructor()
 
+  get priceEvents() {
+    return this.priceEventEmitter
+  } // priceEvents getter
 
-  render() {
-    return (
-      <div>
-        ${this.state.price}
-      </div>
-    )
-  }
+  printSubscriptions(data) {
+    // console.log(data)
+    let timestamp = new Date().toLocaleTimeString()
+    let product = data.channels[0].product_ids[0]
+    let channel = data.channels[0].name
+    let message = `${timestamp} | ${product} | ${channel}`
+    console.log(message)
+  } // printSubscriptions(data)
+
+  printTicker(data) {
+    // console.log(data)
+    let timestamp = new Date(data.time).toLocaleTimeString()
+    let product = data.product_id
+    // let side = data.side
+    let resetColor = "\x1b[0m" // reset
+    let color = resetColor
+    if (data.side === "buy") {
+      color = "\x1b[32m" // green
+    } else if (data.side === "sell"){
+      color = "\x1b[31m" // red
+    }
+    let amount = parseFloat(data.last_size).toFixed(8)
+    let price = parseFloat(data.price).toFixed(2)
+    let message = `${timestamp} | ${product} | ${amount} @ ${color}$${price}${resetColor}`
+    console.log(message)
+  } // printTicker(data)
 }
 
 
