ccbe5b5c90fe46d335465740e34d50f854729286
Matthew Bloom <matthew.j.bloom@gmail.com>
2021-03-14 04:37:10 -0400

    remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import CurrentPrice from './CurrentPrice'
 import NotificationForm from './NotificationForm'
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Crypto Toolkit
         </p>
