4d2cb643c967506e7befb88cb916489a4827a796
Matthew Bloom <matthew.j.bloom@gmail.com>
2021-03-15 20:26:55 -0400

    add first attempt at getting candles

---
+++ public/CoinbaseProAPI.js
@@ -0,0 +1,28 @@
+const https = require('https')
+
+/**
+ * Represents a CoinbasePro API Request
+ *
+ */
+const options = {
+  hostname: 'api.pro.coinbase.com',
+  path: '/products/BTC-USD/candles',
+  method: 'GET',
+  headers: {
+    'User-Agent': 'nodejs'
+  }
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', e => {
+  console.error(e)
+})
+
+req.end()
